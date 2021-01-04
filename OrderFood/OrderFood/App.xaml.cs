bfa4df0af3869d3b1b0a9706940e0f84cac3cc4c
Duc Toan <ductoan20102000@gmail.com>
2021-01-04 11:25:45 +0700

    init commit

---
+++ OrderFood/OrderFood/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using OrderFood.Components;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrderFood
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new NavigationPage(new Login());
+            MainPage =new Login();
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ OrderFood/OrderFood/Components/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrderFood.Components
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            var username = usernameLogin.Text;
+            var password = passwordLogin.Text;
+            if (username == "ad" && password == "ad")
+            {
+                //Navigation.PushAsync(new BottomNavBarXf.Home());
+                Application.Current.MainPage = new BottomNavBarXf.Home();
+
+            }
+            else
+            {
+                DisplayAlert("Notification", "User name or Password incorrect!", "OK");
+            }
+        }
+    }
+}
