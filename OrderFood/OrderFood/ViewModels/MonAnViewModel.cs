bd3dbece8f1d224c740c70452cdc768702062813
Duc Toan <ductoan20102000@gmail.com>
2021-01-19 11:00:37 +0700

    clear comment

--- OrderFood/OrderFood/ViewModels/MonAnViewModel.cs
+++ OrderFood/OrderFood/ViewModels/MonAnViewModel.cs
@@ -32,12 +32,6 @@ namespace OrderFood.ViewModels
             {
                 var navigation = Application.Current.MainPage as NavigationPage;
                 await navigation.PushAsync(new BottomNavBarXf.Home(monAn, "fav"));
-
-                //var httpClient = new HttpClient();
-                //var response = await httpClient.GetStringAsync("http://www.orderfood213.somee.com/api/ServiceController/createHoaDon?MaKH=" + cur);
-
-                //var response2 = await httpClient.GetStringAsync("http://www.orderfood213.somee.com/api/ServiceController/getKhachHangTheoTenDN?TenDN=" + TenDN);
-                //List<User> user = JsonConvert.DeserializeObject<List<User>>(response2);
             }
         }
         public ICommand OrderMonAnCommand => new Command<MonAn>(AddToOrder);
